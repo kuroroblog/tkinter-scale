3ba55c2eff70b3097119ef8fb61649e0787824d1
tocchimittsu <arimagedon21@gmail.com>
2021-07-05 09:29:28 +0900

    修正

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Application(tk.Frame):
     # scaleに関する情報を格納する関数
     scale = None
@@ -13,16 +14,16 @@ class Application(tk.Frame):
         self.master.geometry("300x200")
 
         # Windowを親要素として、frame Widget(Frame)を作成する。
-        # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+        # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
         frame = tk.Frame(self.master)
 
-        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
         # frame Widget(Frame)を親要素として、scale Widgetを作成する。
-        # from_ : 入力できる数字の下限を設定する。初期値とされる。
-        # orient : scale Widgetを水平方向に変更する。
+        # from_ : 選択できる数字の下限を設定する。初期値とされる。
+        # orient : scale Widgetの表示を水平方向へ変更する。
         self.scale = tk.Scale(frame, from_=10, orient=tk.HORIZONTAL)
 
         # frame Widget(Frame)を親要素とした場合に、scale Widgetをどのように配置するのか?
@@ -30,8 +31,9 @@ class Application(tk.Frame):
         self.scale.pack()
 
     # hello worldを出力する関数
-    def helloWorld(self, value):
+    def helloWorld(self):
         print('hello world')
+
 
 # Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
 if __name__ == "__main__":
